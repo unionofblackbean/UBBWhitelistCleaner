efa18d007da5818da4079bb714892bb837cfaf30
Leung Yau Ming <leungyauming2005@gmail.com>
2020-10-11 10:23:13 +0800

    Create Spigot plugin project with Gradle

---
+++ src/main/java/asia/ubb/whitelistcleaner/WhitelistCleanerPlugin.java
@@ -0,0 +1,12 @@
+package asia.ubb.whitelistcleaner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WhitelistCleanerPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
