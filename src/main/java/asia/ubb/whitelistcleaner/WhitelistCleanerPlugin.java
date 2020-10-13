1bc2468fe4bc6b18451a08300a73211944d41202
Leung Yau Ming <leungyauming2005@gmail.com>
2020-10-13 20:27:40 +0800

    Log to file when player is removed from whitelist

--- src/main/java/asia/ubb/whitelistcleaner/WhitelistCleanerPlugin.java
+++ src/main/java/asia/ubb/whitelistcleaner/WhitelistCleanerPlugin.java
@@ -6,12 +6,29 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
 
 public class WhitelistCleanerPlugin extends JavaPlugin {
+
+    private final File logFile = new File(getDataFolder(), "log.txt");
+
+    private void saveLog() {
+        if (!logFile.exists()) {
+            try {
+                logFile.createNewFile();
+            } catch (IOException e) {
+                getLogger().log(Level.SEVERE, "Could not save " + logFile.getName() + ".", e);
+            }
+        } else {
+            getLogger().warning("Could not save " + logFile.getName() + " because " + logFile.getName() + " already exists.");
+        }
+    }
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
+        saveLog();
 
         String cleanIntervalStr = getConfig().getString("clean-interval");
         String offlineBeforeCleanStr = getConfig().getString("offline-before-clean");
@@ -23,7 +40,7 @@ public class WhitelistCleanerPlugin extends JavaPlugin {
 
             BukkitScheduler scheduler = getServer().getScheduler();
             scheduler.scheduleSyncRepeatingTask(
-                    this, new CleanTask(this, offlineBeforeClean),
+                    this, new CleanTask(this, offlineBeforeClean, logFile),
                     0L, cleanInterval);
         } else
             throw new IllegalArgumentException("Option clean-interval or offline-before-clean is not found in config.yml.");
--- src/main/java/asia/ubb/whitelistcleaner/task/CleanTask.java
+++ src/main/java/asia/ubb/whitelistcleaner/task/CleanTask.java
@@ -3,33 +3,63 @@ package asia.ubb.whitelistcleaner.task;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.Plugin;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Set;
+import java.util.logging.Level;
 
 public class CleanTask implements Runnable {
 
     private final Plugin plugin;
     private final long offlineBeforeClean;
+    private final File logFile;
 
-    public CleanTask(Plugin plugin, long offlineBeforeClean) {
+    public CleanTask(Plugin plugin, long offlineBeforeClean, File logFile) {
         this.plugin = plugin;
         this.offlineBeforeClean = offlineBeforeClean;
+        this.logFile = logFile;
     }
 
     @Override
     public void run() {
+        // check if whitelist cleaner is enabled
         boolean enabled = plugin.getConfig().getBoolean("enable", false);
         if (enabled) {
-            Date now = new Date();
-            Set<OfflinePlayer> whitelistedPlayers = plugin.getServer().getWhitelistedPlayers();
-            for (OfflinePlayer whitelistedPlayer : whitelistedPlayers) {
-                long offlineTime = (now.getTime() - whitelistedPlayer.getLastPlayed()) / 1000 * 20;
+            try {
+                // open log file
+                FileWriter logFileWriter = new FileWriter(logFile, true);
 
-                if (offlineTime >= offlineBeforeClean && !whitelistedPlayer.isOnline()) {
-                    whitelistedPlayer.setWhitelisted(false);
-                    plugin.getServer().reloadWhitelist();
+                // get current date and time
+                Date now = new Date();
+                // get all whitelisted players
+                Set<OfflinePlayer> whitelistedPlayers = plugin.getServer().getWhitelistedPlayers();
+                // loop through all whitelisted players
+                for (OfflinePlayer player : whitelistedPlayers) {
+                    // calculate player offline time
+                    long offlineTime = (now.getTime() - player.getLastPlayed()) / 1000 * 20;
+                    // check if player offline time is longer than allowed and if player is not online
+                    if (offlineTime >= offlineBeforeClean && !player.isOnline()) {
+                        // remove player from whitelist
+                        player.setWhitelisted(false);
+                        // reload whitelist
+                        plugin.getServer().reloadWhitelist();
+
+                        // convert current time to string
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                        String time = sdf.format(now);
+                        // log removal details to file
+                        plugin.getLogger().info("asdasdasdasd");
+                        logFileWriter.write("[" + time + "] " + player.getName() + " (" + player.getUniqueId() + ")" + System.lineSeparator());
+                    }
                 }
 
+                // close log file
+                logFileWriter.close();
+            } catch (IOException e) {
+                plugin.getLogger().log(Level.SEVERE, "Could not open or write to " + logFile.getName() + ".", e);
             }
         }
     }
