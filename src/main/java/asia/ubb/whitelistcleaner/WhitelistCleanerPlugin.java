02e419c6ddc06e5a16b7d69ea84f57ad67e04a7c
Leung Yau Ming <leungyauming2005@gmail.com>
2020-10-13 16:11:04 +0800

    Schedule whitelist clean task when plugin is enabled

--- src/main/java/asia/ubb/whitelistcleaner/WhitelistCleanerPlugin.java
+++ src/main/java/asia/ubb/whitelistcleaner/WhitelistCleanerPlugin.java
@@ -1,12 +1,36 @@
 package asia.ubb.whitelistcleaner;
 
+import asia.ubb.whitelistcleaner.task.CleanTask;
+import asia.ubb.whitelistcleaner.utils.TicksUtils;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import java.io.File;
 
 public class WhitelistCleanerPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        File dataFolder = getDataFolder();
+        if (!dataFolder.exists())
+            if (!dataFolder.mkdirs())
+                throw new RuntimeException("Cannot create data folder.");
+        saveDefaultConfig();
 
+        String cleanIntervalStr = getConfig().getString("clean-interval");
+        String offlineBeforeCleanStr = getConfig().getString("offline-before-clean");
+        if (cleanIntervalStr != null && offlineBeforeCleanStr != null) {
+            long cleanInterval = TicksUtils.parseTicks(cleanIntervalStr);
+            long offlineBeforeClean = TicksUtils.parseTicks(offlineBeforeCleanStr);
+            getLogger().info("Clean Interval (Seconds):         " + cleanInterval / 20);
+            getLogger().info("Offline Before Clean (Seconds):   " + offlineBeforeClean / 20);
+
+            BukkitScheduler scheduler = getServer().getScheduler();
+            scheduler.scheduleSyncRepeatingTask(
+                    this, new CleanTask(this, offlineBeforeClean),
+                    0L, cleanInterval);
+        } else
+            throw new IllegalArgumentException("Option clean-interval or offline-before-clean is not found in config.yml.");
     }
 
 }
